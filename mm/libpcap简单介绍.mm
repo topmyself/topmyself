9c34012d4177fb029cdf22768740f8ff0a2a7069
zhaodl <zhaodl@knownsec.com>
2016-08-23 15:07:58 +0800

    add document and mm file

---
+++ mm/libpcap简单介绍.mm
@@ -0,0 +1,21 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1470989996365" ID="ID_1227015963" MODIFIED="1470990261353">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font size="5">libpcap&#31616;&#21333;&#20171;&#32461;</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1470990011002" ID="ID_1376526263" MODIFIED="1470990282555" POSITION="right" TEXT="1. api&#x8be6;&#x7ec6;&#x4ecb;&#x7ecd;"/>
+<node CREATED="1470990041023" ID="ID_1405349572" MODIFIED="1470990289040" POSITION="right" TEXT="2. libpcap&#x6837;&#x4f8b;"/>
+<node CREATED="1470990068096" ID="ID_1521251581" MODIFIED="1470990293523" POSITION="right" TEXT="3. libpcap&#x5e95;&#x5c42;&#x5b9e;&#x73b0;&#x673a;&#x5236;"/>
+<node CREATED="1470990084025" ID="ID_1844054540" MODIFIED="1470990297845" POSITION="left" TEXT="4. libpcap&#x5e94;&#x7528;&#x4e4b;tcpdump"/>
+<node CREATED="1470990101332" ID="ID_852225624" LINK="wireshark&#x7b80;&#x4ecb;.mm" MODIFIED="1470994386188" POSITION="left" TEXT="5. libpcap&#x5e94;&#x7528;&#x4e4b;wireshark"/>
+</node>
+</map>
---
+++ mm/wireshark简介.mm
@@ -0,0 +1,24 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1470994040584" ID="ID_1666387010" MODIFIED="1470994334651">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font size="5">wireshark&#31616;&#20171;</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1470994098543" ID="ID_432563836" MODIFIED="1470994111112" POSITION="right" TEXT="epan&#x534f;&#x8bae;&#x89e3;&#x6790;">
+<node CREATED="1470994114728" ID="ID_1427381444" MODIFIED="1470994116284" TEXT="http"/>
+<node CREATED="1470994240845" ID="ID_386185501" MODIFIED="1470994244666" TEXT="&#x5176;&#x4ed6;&#x534f;&#x8bae;"/>
+</node>
+<node CREATED="1470994158493" ID="ID_1188062100" MODIFIED="1470994169724" POSITION="left" TEXT="wireshark&#x6293;&#x5305;&#x673a;&#x5236;"/>
+<node CREATED="1470994338477" ID="ID_289731443" MODIFIED="1470994349990" POSITION="right" TEXT="wiretap&#x89e3;&#x8bfb;pcap&#x6587;&#x4ef6;"/>
+<node CREATED="1470994570966" ID="ID_1301458025" MODIFIED="1470994576625" POSITION="left" TEXT="wireshark&#x53d1;&#x5305;&#x673a;&#x5236;"/>
+<node CREATED="1470994678869" ID="ID_1139404218" MODIFIED="1470994688634" POSITION="right" TEXT="bpf&#x8fc7;&#x6ee4;&#x8bed;&#x6cd5;"/>
+</node>
+</map>
